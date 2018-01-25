bbddc2f5de04dedaec378153ee5064b1f221d57b
JooChan <14jshin@gmail.com>
2018-01-25 12:49:21 -0500

    Added shaders

--- second/index.js
+++ second/index.js
@@ -1,3 +1,23 @@
+var vertexShaderText = 
+['percision mediump float;',
+'',
+'attribute vec2 vertPosition;',
+'',
+'void main()',
+'{',
+'	gl_Position = vec4(vertPosition, 0.0, 1.0);',
+'}'
+].join('\n');
+
+var fragmentShaderText = 
+['percision mediump float;',
+'',
+'void main()',
+'{',
+'	gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);',
+'}'
+].join('\n');
+
 var initDemo = function(){
 	var canvas = document.getElementById("game-surface");
 	var gl = canvas.getContext("webgl");
@@ -12,5 +32,8 @@ var initDemo = function(){
 	// canvas.width = window.innerWidth;
 	// canvas.height = window.innerHeight;
 	gl.clearColor(0.75, 0.85, 0.8, 1.0);//	R, G, B, Alpha
-	gl.clear();
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+	var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+	var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
 }
